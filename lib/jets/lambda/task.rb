142677b11349b4f021f86b514238439e03a7b6a5
Tung Nguyen <tongueroo@gmail.com>
2018-09-08 02:35:53 +0000

    wip: private meth fix for private methods defined later

--- lib/jets/lambda/task.rb
+++ lib/jets/lambda/task.rb
@@ -13,12 +13,18 @@ class Jets::Lambda::Task
     @lang = options[:lang] || :ruby
   end
 
-  def build_function_iam?
-    !!(@iam_policy || @managed_iam_policy)
-  end
-
   def name
     @meth
+  end
+
+  def public_meth?
+    klass = @class_name.constantize
+    public_methods = klass.public_instance_methods - Object.methods
+    public_methods.include?(meth.to_sym)
+  end
+
+  def build_function_iam?
+    !!(@iam_policy || @managed_iam_policy)
   end
 
   @@lang_exts = {
